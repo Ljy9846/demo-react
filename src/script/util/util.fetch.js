37a9b1e73b98085f2310340f2b04a2ab70e35876
Ljy9846 <984632120@qq.com>
2017-03-29 09:18:10 +0800

    beibei

---
+++ src/script/util/util.fetch.js
@@ -0,0 +1,11 @@
+
+/*封装好的fect*/
+let fetchData = (url, callback) => {
+  fetch(url)
+    .then(response=>response.json())
+    .then(res=>{
+      callback(res)
+    })
+}
+
+export default fetchData
