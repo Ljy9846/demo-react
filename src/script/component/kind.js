f0531ed2847001881981c1196e0de1e300e5c322
jack91040 <513110007@qq.com>
2017-03-29 22:04:57 +0800

    git add .

--- src/script/component/kind.js
+++ src/script/component/kind.js
@@ -27,7 +27,7 @@ class Kind1 extends React.Component {
     )
   } 
   componentDidMount(){
-    let url1='/api/kind.php'
+    let url1='/api/category/categorylist?device=pc&channel=h5&swidth=1280&sheight=800&zoneId=857&v=2.1.3&terminal=wap&page=http%3A%2F%2Fm.haoshiqi.net%2F%23category%3Fchannel_id%3Dh5'
     fetchData(url1,(res)=>{
       // let lis1=res.data.list.map(val=>{
       //   return val.name
@@ -38,7 +38,7 @@ class Kind1 extends React.Component {
       //     })
       // })
       let lis3=res.data.list.map(val=>{
-        return (<div><h4>{val.name}<i className="yo-ico">&#xe60c;</i></h4><ul>{val.subCategories.map(sol=>{
+        return (<div className="clearfix"><h4>{val.name}<i className="yo-ico">&#xe60c;</i></h4><ul>{val.subCategories.map(sol=>{
           return (<li>{sol.name}</li>)
         })}</ul></div>)
       })
